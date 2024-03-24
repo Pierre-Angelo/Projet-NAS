0b900837982675c24bc8e9934f42f7473d9d7207
Venice-pasha <155968373+Venice-pasha@users.noreply.github.com>
2024-03-24 17:19:17 +0100

    Add files via upload

--- generate_config.py
+++ generate_config.py
@@ -10,6 +10,35 @@ intent_file_name = ".\\intent_file_net_policies.json" #sys.argv[1]
 with open(intent_file_name,'r',encoding='utf-8') as f:
  data = list(json.load(f).values())
 
+def get_sr(routeurs):
+    routeur_serveur=[]
+    for i,router in enumerate(routeurs):
+        if router["AS"]==1:
+            routeur_serveur.append(router["hostname"])
+    return(routeur_serveur)
+
+def icon(routeurs,routeur_serveur):
+    connexion_list=[]
+    for i,router in enumerate(routeurs):
+        hostname=router["hostname"]
+        for interface,value in router["interfaces"].items():
+             if (value in routeur_serveur) and (([value,hostname]) not in connexion_list):
+                  connexion_list.append([hostname,value])
+    return(connexion_list)
+
+def i_autoip(routeurs,connexion_list):
+    for i,router in enumerate(routeurs):
+        hostname=router["hostname"]
+        for interface,value in router["interfaces"].items():
+            if [hostname,value] in connexion_list:
+                index = connexion_list.index([hostname, value])
+                ip=(f"192.168.{index+128}.1/30")
+                routeurs[i]["interfaces"][interface]=ip
+            elif [value,hostname] in connexion_list:
+                index = connexion_list.index([value,hostname])
+                ip=(f"192.168.{index+128}.2/30")
+                routeurs[i]["interfaces"][interface]=ip
+
 def mask(i) :
     if (i == 32) :
         return "255.255.255.255"
@@ -150,6 +179,10 @@ class router() :
                     type_nei.append(inter[1])
         return res
 
+
+routeur_serveur=get_sr(data)
+connexion_list=icon(data,routeur_serveur)
+i_autoip(data,connexion_list)
 
 list_router = []
 
