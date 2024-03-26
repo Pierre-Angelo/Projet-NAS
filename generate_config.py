be0f5aa8a84d5c7bda911e57cf7ebbab2380a31f
Rolgarel <raphael.dembik@gmail.com>
2024-03-26 15:30:28 +0100

    correction bug iBGP
    bug : routeur qui cherche à communiquer avec lui-même

--- generate_config.py
+++ generate_config.py
@@ -73,7 +73,8 @@ class router() :
         # i-bgp
         if self.border != "NULL" :
             for nei in listRAS :
-                if nei != self.hostname and nei[1] != "NULL":
+                print(nei[1][0])
+                if nei[0] != self.hostname and nei[1] != "NULL":
                     res+= " neighbor "+self.AS+"."+nei[0][2]+".0.1"+" remote-as "+ self.AS+"\n"
                     res+= " neighbor "+self.AS+"."+nei[0][2]+".0.1"+" update-source Loopback0\n"
         
